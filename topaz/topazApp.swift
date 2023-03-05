7e8c921c93c0d151d33e607ffcd15a8e6f783f03
Tanner Silva <t@tannersilva.com>
2023-03-05 10:35:23 -0600

    initial commit

---
+++ topaz/topazApp.swift
@@ -0,0 +1,17 @@
+//
+//  topazApp.swift
+//  topaz
+//
+//  Created by Tanner Silva on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct topazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
